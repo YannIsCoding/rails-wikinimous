12002d6e2b7cdb96e91fa74d02aed85dfefe25cd
Yann Parent <yann.parent4@gmail.com>
2019-10-07 18:02:33 +0200

    Basic CRUD fonction implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  # get 'articles/index'
+  # get 'articles/show'
+  # get 'articles/new'
+  # get 'articles/create'
+  # get 'articles/update'
+  # get 'articles/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
+  root to: 'articles#index'
 end
+
+
